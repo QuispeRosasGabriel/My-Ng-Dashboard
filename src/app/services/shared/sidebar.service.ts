6a291ec2d836a3ea820c87496e8703cf5e07b775
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-04-18 22:05:49 -0500

    mock sidebar service

--- src/app/services/shared/sidebar.service.ts
+++ src/app/services/shared/sidebar.service.ts
@@ -3,6 +3,17 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SidebarService {
 
+  menu: Array<any> = [
+    {
+      titulo: "Principal", icono: "mdi mdi-gauge",
+      submenu: [
+        { titulo: "Dashboard", url: "/dashboard" },
+        { titulo: "Progress", url: "/progress" },
+        { titulo: "Gráficas", url: "/graficas1" }
+      ]
+    },
+  ]
+
   constructor() { }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,25 +1,28 @@
 import { NgModule } from '@angular/core';
-
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
-    declarations: [
-        NopagefoundComponent,
-        HeaderComponent,
-        SidebarComponent,
-        BreadcrumbsComponent,
-        NopagefoundComponent
-    ],
-    exports: [
-        NopagefoundComponent,
-        HeaderComponent,
-        SidebarComponent,
-        BreadcrumbsComponent,
-        NopagefoundComponent
-    ]
+  imports: [
+    RouterModule
+  ],
+  declarations: [
+    NopagefoundComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    NopagefoundComponent
+  ],
+  exports: [
+    NopagefoundComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    NopagefoundComponent
+  ]
 })
 export class SharedModule { }
