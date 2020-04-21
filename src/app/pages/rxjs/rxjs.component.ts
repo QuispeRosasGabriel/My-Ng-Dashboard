c477ed196b0f6377f189fe074f52be8d0507630e
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-04-20 19:12:31 -0500

    finishing observables and promises

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { retry } from "rxjs/operators"
+import { retry, map, filter } from "rxjs/operators"
 
 @Component({
   selector: 'app-rxjs',
@@ -11,32 +11,45 @@ export class RxjsComponent implements OnInit {
 
   constructor() {
 
-    this.regresaObservable().pipe(
-      retry(2)
-    ).subscribe(data => console.log(data),
-      error => console.error(error),
-      () => console.log("observador termino")
-    )
+    this.regresaObservable()
+      .subscribe(data => console.log(data),
+        error => console.error(error),
+        () => console.log("observador termino")
+      )
   }
 
   ngOnInit() {
   }
 
-  regresaObservable(): Observable<number> {
+  regresaObservable(): Observable<any> {
     return new Observable(observer => {
       let contador = 0;
       let intervalo = setInterval(() => {
         contador += 1
-        observer.next(contador);
+
+        const salida = {
+          valor: contador
+        }
+
+
+        observer.next(salida);
         if (contador === 3) {
           clearInterval(intervalo);
           observer.complete();
         }
-        if (contador === 2) {
-          // clearInterval(intervalo);
-          observer.error("Auxilio")
-        }
+        // if (contador === 2) {
+        //    clearInterval(intervalo);
+        //   observer.error("Auxilio")
+        // }
       }, 1000)
-    });
+    }).pipe(
+      map((resp) => resp = 10),
+      filter((valor, index) => {
+        if ((valor % 2) === 1) {
+          return true;
+        } else {
+          return false
+        }
+      }))
   }
 }
