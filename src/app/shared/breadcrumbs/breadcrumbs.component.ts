b201edb7fbd4590ace011c698fa1cd91ac64b9d2
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-04-20 22:37:01 -0500

    setting reactive title for the pages

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class BreadcrumbsComponent implements OnInit {
+  titulo: string;
 
-  constructor() { }
+  constructor(private router: Router, private title: Title) {
+    this.getDataRoute().subscribe(event => {
+      this.titulo = event.titulo;
+      this.title.setTitle(this.titulo);
+    })
+  }
 
   ngOnInit() {
+  }
+
+  getDataRoute() {
+    return this.router.events.pipe(
+      filter(data => data instanceof ActivationEnd),
+      filter((data: ActivationEnd) => data.snapshot.firstChild === null),
+      map((data: ActivationEnd) => data.snapshot.data)
+    )
+
   }
 
 }
