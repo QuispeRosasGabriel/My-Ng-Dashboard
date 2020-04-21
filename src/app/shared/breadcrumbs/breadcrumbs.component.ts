aa505cd1d0fe6a26790db0697a04fc626e0602cf
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-04-20 22:44:11 -0500

    working with meta tags

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
-import { Title } from '@angular/platform-browser';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -11,10 +11,16 @@ import { Title } from '@angular/platform-browser';
 export class BreadcrumbsComponent implements OnInit {
   titulo: string;
 
-  constructor(private router: Router, private title: Title) {
+  constructor(private router: Router, private title: Title, private meta: Meta) {
     this.getDataRoute().subscribe(event => {
       this.titulo = event.titulo;
       this.title.setTitle(this.titulo);
+
+      const metaTag: MetaDefinition = {
+        name: "description",
+        content: this.titulo,
+      }
+      this.meta.updateTag(metaTag);
     })
   }
 
