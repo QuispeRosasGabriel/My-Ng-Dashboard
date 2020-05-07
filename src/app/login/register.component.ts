7903e80d238de5817d7ce3af6a7c8bc6a63c6f04
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-05-06 21:57:52 -0500

    finish register module

--- src/app/login/register.component.ts
+++ src/app/login/register.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import swal from 'sweetalert'
-
+import { UsuarioService } from '../services/service.index';
+import { Usuario } from '../models/usuario.model';
+import { Router } from '@angular/router';
 
 declare function init_plugins()
 
@@ -14,7 +16,7 @@ export class RegisterComponent implements OnInit {
 
   forma: FormGroup;
 
-  constructor() { }
+  constructor(private _usuarioService: UsuarioService, public router: Router) { }
 
   ngOnInit() {
     init_plugins();
@@ -45,6 +47,17 @@ export class RegisterComponent implements OnInit {
       swal("Importante!", "Debe aceptar las condiciones", "warning");
       return;
     }
+    let usuario = new Usuario(
+      this.forma.value.nombre,
+      this.forma.value.correo,
+      this.forma.value.password,
+    )
+
+    this._usuarioService.crearUsuario(usuario)
+      .subscribe(resp => {
+        console.log(resp);
+        this.router.navigate(['/login'])
+      })
   }
 
   sonIguales(campo1: string, campo2: string) {
