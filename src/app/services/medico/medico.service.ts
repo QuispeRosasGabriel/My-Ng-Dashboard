45e0c0de2cffac1ba27c073d38845f7f6efb233a
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-05-18 22:00:43 -0500

    armando logica para medicos

--- src/app/services/medico/medico.service.ts
+++ src/app/services/medico/medico.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { URL_SERVICIOS } from '../../config/constants';
+
 
 @Injectable()
 export class MedicoService {
+  totalMedicos: number = 0
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+
+  cargarMedicos() {
+    let url = URL_SERVICIOS + "/medico"
+    return this.http.get(url)
+      .map((resp: any) => {
+        this.totalMedicos = resp.total
+        return resp.medicos
+      }
+      )
+  }
 
 }
