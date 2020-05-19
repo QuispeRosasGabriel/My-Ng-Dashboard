1b96a219851a24f71d2f2cc8be895757b335f97d
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-05-18 21:26:26 -0500

    hospitales component terminado

--- src/app/pages/hospitales/hospitales.component.ts
+++ src/app/pages/hospitales/hospitales.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Hospital } from '../../models/hospital.model';
 import { HospitalService } from '../../services/service.index';
+import { ModalUploadService } from '../../components/modal-upload/modal-upload.service';
+
+declare var swal: any
 
 @Component({
   selector: 'app-hospitales',
@@ -11,10 +14,14 @@ export class HospitalesComponent implements OnInit {
   hospitales: Hospital[] = []
 
 
-  constructor(private _hospitalService: HospitalService) { }
+  constructor(private _hospitalService: HospitalService,
+    private _modalUploadService: ModalUploadService
+  ) { }
 
   ngOnInit() {
     this.cargarHospitales()
+    this._modalUploadService.notificacion
+      .subscribe(() => { this.cargarHospitales() })
   }
 
   cargarHospitales() {
@@ -39,6 +46,27 @@ export class HospitalesComponent implements OnInit {
   borrarHospital(hospital: Hospital) {
     this._hospitalService.borrarHospital(hospital._id)
       .subscribe(() => { this.cargarHospitales() })
+  }
+
+  crearHospital() {
+    swal({
+      title: "Crear Hospital",
+      text: "Ingrese el nombre del hospital",
+      content: "input",
+      icon: "info",
+      buttons: true,
+      dangerMode: true
+    }).then(valor => {
+      if (!valor && valor.length === 0) {
+        return
+      }
+      this._hospitalService.crearHospital(valor)
+        .subscribe(() => { this.cargarHospitales() })
+    })
+  }
+
+  actualizarImagen(hospital: Hospital) {
+    this._modalUploadService.mostrarModal("hospitales", hospital._id);
   }
 
 }
